4ed9fbd59379dab4fd58f67f0117601b7b9b5741
Natalya Filatenkova <Filatenkova87@yandex.ru>
2021-05-01 17:16:02 +0300

    VUEJS / страницы подключены к роутеру, настроен вывод общего шаблона страниц

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
+
+<script>
+// eslint-disable-next-line import/extensions
+import EmptyLayout from '@/layouts/EmptyLayout';
+// eslint-disable-next-line import/extensions
+import MainLayout from '@/layouts/MainLayout';
+
+export default {
+  computed: {
+    layout() {
+      return `${this.$route.meta.layout || 'empty'}-layout`;
+    },
+  },
+  components: {
+    EmptyLayout, MainLayout,
+  },
+};
+</script>
 
 <style lang="scss">
 @import '~materialize-css/dist/css/materialize.min.css';
