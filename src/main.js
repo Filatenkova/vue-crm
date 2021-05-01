cf238deff4afa6c184d0fec591cf81813fe0f6e3
Natalya Filatenkova <Filatenkova87@yandex.ru>
2021-05-01 22:04:19 +0300

    VUEJS / динамический вывод времени в панели навигации

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+// eslint-disable-next-line import/order,import/extensions,import/no-unresolved
+import dateFilter from 'filters/date.filter.js';
 import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
+Vue.filter('dateF', dateFilter);
 
 new Vue({
   router,
