156e5ddc833d5ceef041773e20da99e142bca3f3
Natalya Filatenkova <Filatenkova87@yandex.ru>
2021-05-02 08:59:03 +0300

    VUEJS / валидация полей формы

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate/src';
 import App from './App.vue';
-import './registerServiceWorker';
 import router from './router';
 import store from './store';
 // eslint-disable-next-line import/order,import/extensions,import/no-unresolved
 import dateFilter from './filters/date.filter.js';
+import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 Vue.filter('dateF', dateFilter);
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
@@ -7,18 +7,42 @@
             id="email"
             type="text"
             class="validate"
+            v-model.trim="email"
+            :class="{
+              // eslint-disable-next-line max-len
+              invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)
+            }"
         >
-        <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <label for="email">E-mail</label>
+        <small
+          v-if="$v.email.$dirty && !$v.email.required"
+          class="helper-text invalid">Поле e-mail не должно быть пустым
+        </small>
+        <small
+          v-else-if-="$v.email.$dirty && !$v.email.email"
+          class="helper-text invalid">Введите корректный e-mail
+        </small>
       </div>
       <div class="input-field">
         <input
             id="password"
             type="password"
-            class="validate"
+            v-model.trim="password"
+            :class="{
+              // eslint-disable-next-line max-len
+              invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)
+            }"
         >
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          v-if="$v.password.$dirty && !$v.password.required"
+          class="helper-text invalid">Введите пароль
+        </small>
+        <small
+          v-else-if="$v.password.$dirty && !$v.password.minLength"
+          class="helper-text invalid">
+          Пароль должен быть не менее {{ $v.password.$params.minLength.min }} символов
+        </small>
       </div>
     </div>
     <div class="card-action">
@@ -34,8 +58,42 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+
+import { email, required, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'login',
+  date: () => ({
+    email: '',
+    password: '',
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+  },
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      console.log(formData);
+
+      this.$router.push('/');
+    },
+  },
+};
+</script>
